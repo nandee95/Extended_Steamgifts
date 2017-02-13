cddb3e811b69ae6f214c5b332b7ce28d659a2f68
Murat L <wineliva@gmail.com>
2017-02-13 05:00:03 +0200

    Font fix

--- Extended_Steamgifts_Enter_All.user.js
+++ Extended_Steamgifts_Enter_All.user.js
@@ -16,7 +16,7 @@
 
 if(window.location=="https://www.steamgifts.com/giveaways/search?type=wishlist")
 {
-    $(".page__heading__breadcrumbs:first").after('<div class="sidebar__entry-insert enterall" style="cursor:pointer"><i style="margin-left:5px;display:inline" class="fa fa-plus-circle"> Enter all</div>');
+    $(".page__heading__breadcrumbs:first").after('<div class="sidebar__entry-insert enterall" style="cursor:pointer"><i style="margin-left:5px;display:inline" class="fa fa-plus-circle"></i> Enter all</div>');
     $(document).on('click', '.enterall', function() {
         $(".sidebar__entry-insert:not(.is-hidden):not(.enterall)").each(function () {
             if($(this).closest('.pinned-giveaways__inner-wrap').length===0) $(this).trigger('click');
